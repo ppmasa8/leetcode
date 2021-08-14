ebf0a57cb8f474ee5659605714bc820a37318f06
ppmasa8 <kiyokiyopip56@gmail.com>
2021-08-14 12:30:51 +0900

    Add

---
+++ explore/top-interview-questions/easy/RotateArray.rb
@@ -0,0 +1,7 @@
+# @param {Integer[]} nums
+# @param {Integer} k
+# @return {Void} Do not return anything, modify nums in-place instead.
+def rotate(nums, k)
+    k.times{nums.unshift(nums.pop)}
+    nums
+end
