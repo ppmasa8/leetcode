2838282230a3c24209aa6d074946dbbb67ad945c
ppmasa8 <kiyokiyopip56@gmail.com>
2021-08-18 18:00:55 +0900

    Add

---
+++ explore/top-interview-questions/easy/linked-list/ReverseLinkedList.rb
@@ -0,0 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode} head
+# @return {ListNode}
+def reverse_list(head)
+    current = head; prev = nil;
+    while current
+        temp = current.next; current.next = prev; prev = current; current = temp;
+    end
+    return prev
+end
