f62128b859f74a4a0f909ddc41aa499dae6eff2a
ppmasa8 <kiyokiyopip56@gmail.com>
2021-08-19 21:42:50 +0900

    Add

---
+++ explore/top-interview-questions/easy/sorting-and-searching/MergeSortedArray.rb
@@ -0,0 +1,9 @@
+# @param {Integer[]} nums1
+# @param {Integer} m
+# @param {Integer[]} nums2
+# @param {Integer} n
+# @return {Void} Do not return anything, modify nums1 in-place instead.
+def merge(nums1, m, nums2, n)
+    nums1.slice!(m...nums1.size)
+    nums1.push(nums2).flatten!.sort!
+end
