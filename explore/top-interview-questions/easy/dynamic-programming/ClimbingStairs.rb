b9b01fafda362dab884e139b586d70635cf1a9c0
ppmasa8 <kiyokiyopip56@gmail.com>
2021-08-26 14:20:13 +0900

    ad

---
+++ explore/top-interview-questions/easy/dynamic-programming/ClimbingStairs.rb
@@ -0,0 +1,10 @@
+# @param {Integer} n
+# @return {Integer}
+def climb_stairs(n)
+    return 1 if n == 1
+    cache = [1, 2]
+    (n - 2).times do
+      cache.push(cache[-2] + cache[-1])
+    end
+    cache[-1]
+  end
