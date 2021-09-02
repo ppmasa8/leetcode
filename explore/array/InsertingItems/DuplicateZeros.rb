9bf8d64d681130e138fe9fc62aa7971bcaf03606
ppmasa8 <kiyokiyopip56@gmail.com>
2021-09-02 22:33:07 +0900

    ass

---
+++ explore/array/InsertingItems/DuplicateZeros.rb
@@ -0,0 +1,13 @@
+# @param {Integer[]} arr
+# @return {Void} Do not return anything, modify arr in-place instead.
+def duplicate_zeros(arr)
+    num = arr.size-1; i = 0
+    while i<num
+       if arr[i] == 0
+           arr[i] = [0, 0]
+           arr.pop; i+=1; arr.flatten!
+       end
+    i+=1
+    end
+    arr
+end
