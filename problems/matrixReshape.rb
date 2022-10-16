13e769566f7f2b2f9094dbaead0349bc9bc4debb
ppmasa8 <kiyokiyopip56@gmail.com>
2022-10-17 00:22:31 +0900

    Create matrixReshape.rb

---
+++ problems/matrixReshape.rb
@@ -0,0 +1,11 @@
+# @param {Integer[][]} mat
+# @param {Integer} r
+# @param {Integer} c
+# @return {Integer[][]}
+def matrix_reshape(mat, r, c)
+    arr = mat.flatten
+    return mat if arr.size != r * c
+    res = []
+    (0...r).each{|i| res << arr[i*c...(i+1)*c]}
+    res
+end
